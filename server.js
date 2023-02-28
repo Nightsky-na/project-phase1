9f094a02824f564e129d98710aedd14feba39fe6
Kontawat Wisetpaitoon <70422920+Nightsky-na@users.noreply.github.com>
2023-02-28 23:19:07 +0700

    Update PORT variable to 8080

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(errorHandler);
 // --------------- MIDDLEWARE ----------------
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
